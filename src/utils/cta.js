5c10f57e61b0d0c54b0fde644ed25c6de4364443
axc-project <avi@axellaresearch.com>
2026-02-22 21:53:50 -0500

    Delete src directory

--- src/utils/cta.js
+++
@@ -1,5 +0,0 @@
-export function buildMailto({ email, subject, body }) {
-  const s = encodeURIComponent(subject || "");
-  const b = encodeURIComponent(body || "");
-  return `mailto:${email}?subject=${s}&body=${b}`;
-}
