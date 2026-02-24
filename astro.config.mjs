107b3298ff553f82626ecf01a31cf7cee361bab9
axc-project <avi@axellaresearch.com>
2026-02-23 19:28:29 -0500

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  site: "https://historical-translation-project-site.vercel.app",
-  integrations: [tailwind()],
-  output: "static",
-});
